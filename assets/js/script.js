84884603bfb6c7d939485feea74d7cc80bc33ebd
tripdog <eflips@yahoo.com>
2022-01-16 18:20:18 -0800

    Form ajax, css etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,15 @@ $(window).on('load', function () {
 	});
 
 })(jQuery);
+
+const handleSubmit = (e) => {
+	e.preventDefault()
+	let myForm = document.getElementById('contForm');
+	let formData = new FormData(myForm)
+	fetch('/', {
+		method: 'POST',
+		headers: { "Content-Type": "application/x-www-form-urlencoded" },
+		body: new URLSearchParams(formData).toString()
+	}).then(() => console.log('Form successfully submitted')).catch((error) =>
+		alert(error))
+}
